7759aca03839642599a41ba67f8504a874911145
Shovan Akhuli <shovanakhuli@gmail.com>
2024-12-03 01:21:32 +0530

    Corrected enities services and repo

--- src/main/java/com/example/airBnb/ServicesImpl/BookingServiceImpl.java
+++ src/main/java/com/example/airBnb/ServicesImpl/BookingServiceImpl.java
@@ -3,7 +3,6 @@ package com.example.airBnb.ServicesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.airBnb.Entities.Booking;
 import com.example.airBnb.Repositories.BookingRepository;
 import com.example.airBnb.Services.BookingService;
 
